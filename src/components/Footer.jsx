585db3d03b115cf596de4f3ebda71a9dcb483dcd
unknown <fuaad29@gmail.com>
2024-02-05 12:39:49 +0500

    code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,30 @@
 function Footer() {
 
   return (
-    <div className="flex">
-      <div className="flex w-1/4">
-        <p> 2023 foodpanda</p>
+    <div className="flex text-[1rem] text-gray font-[400] p-12">
+      <div className="flex w-1/3">
+        <p> &copy; 2023 foodpanda</p>
       </div>
-      <div className="flex w-1/4">
+      <div className="flex flex-col w-1/3">
         <span>Press</span>
         <span>Help center</span>
         <span>Refunds with pandapay</span>
-        <span>Pandapay User Account Terms and Conditions</span>
+        <span>Pandapay User Account</span>
         <span>Terms and Conditions</span>
         <span>Privacy Policy</span>
         <span>Security</span>
+        <span>Download Foodpanda Apps</span>
+        <span>Human Rights Policy</span>
+
+      </div>
+      <div className="flex flex-col w-1/3">
+        <span>Careers</span>
+        <span>All cusines</span>
+        <span>Listed cities</span>
+        <span>User Account</span>
+        <span>Cashback</span>
+        <span>Complaints</span>
+        <span>Feedback</span>
         <span>Download Foodpanda Apps</span>
         <span>Human Rights Policy</span>
 
