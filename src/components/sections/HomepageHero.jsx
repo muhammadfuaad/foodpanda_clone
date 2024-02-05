0613f0605417e1b0e6c12aaeb137544c1faf816a
unknown <fuaad29@gmail.com>
2024-02-05 12:03:40 +0500

    code

--- src/components/sections/HomepageHero.jsx
+++ src/components/sections/HomepageHero.jsx
@@ -1,11 +1,11 @@
 function HomepageHero() {
 
   return (
-    <div className="flex">
-      <div>
-        <p>It's the food and groceries you love, delivered</p>
-        <div className="flex bg-white p-4">
-          <div className="bg-white flex outline outline-gray rounded-lg">
+    <div className="flex" id="hero">
+      <div className="w-[55%]">
+        <p className="text-[1.75rem] font-medium text-gray mt-32">It's the food and groceries you love, delivered</p>
+        <div className="flex gap-4 mt-6 bg-white p-4 rounded-lg">
+          <div className="bg-white flex outline outline-1 outline-[#bdbdbd] rounded-lg">
             <input className=""></input>
             <button className="flex items-center gap-2 bg-transparent">
               <img src="./images/worldwide.png" className="w-4"></img>
@@ -15,9 +15,7 @@ function HomepageHero() {
           <button className="bg-primary text-white">Find food</button>
         </div>
       </div>
-      <div id="hero">
-
-      </div>
+      
     </div>
   )
 }
--- src/components/sections/SecondSection.jsx
+++ src/components/sections/SecondSection.jsx
@@ -4,7 +4,7 @@ function SecondSection() {
 
   return (
     <div className="h-[100vh]">
-      <p className="text-black text-[1.5rem] font-medium mb-4">You prepare the food, we handle the rest</p>
+      <p className="text-black text-[1.5rem] font-medium mb-4 mt-12">You prepare the food, we handle the rest</p>
       <div id="second_section" className="relative">
         <div className="absolute top-[60%] w-[65%]">
           <Register />
