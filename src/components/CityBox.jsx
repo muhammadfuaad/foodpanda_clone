ffc8be460a6786dec0ddb2bf60c0cca4c3cdc83b
unknown <fuaad29@gmail.com>
2024-02-05 12:46:01 +0500

    code

--- src/components/CityBox.jsx
+++ src/components/CityBox.jsx
@@ -3,7 +3,7 @@
 function CityBox() {
 
   return (
-    <div className="flex justify-between border border-black rounded-xl h-48 w-1/4 relative">
+    <div className="flex justify-between rounded-xl h-48 w-1/4 relative" id="city_box">
       <img src="./images/cities/lahore.jpg"></img>
       <div className="absolute bg-white rounded-lg p-4 bottom-2 left-2">Lahore</div>
     </div>
--- src/components/sections/CitiesSection.jsx
+++ src/components/sections/CitiesSection.jsx
@@ -4,8 +4,8 @@ import CityBox from "../CityBox"
 function CitiesSection() {
 
   return (
-    <div className="flex flex-col gap-6">
-      <p className="text-gray text-[1.5rem] font-medium mb-4 px-12">Find us in these cities and many more!</p>
+    <div className="flex flex-col gap-6 px-12">
+      <p className="text-gray text-[1.5rem] font-medium mb-4">Find us in these cities and many more!</p>
       <div className="flex gap-6">
         <CityBox />
         <CityBox />
